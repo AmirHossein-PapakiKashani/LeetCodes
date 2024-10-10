05c81722dd2a330364cec7a6898dc026acbf3b87
Amir <amirhosseinpapaki@gmail.com>
2024-10-10 21:14:55 +0330

    solved leetcode 724

---
+++ LeetCode724.py
@@ -0,0 +1,21 @@
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        next = 0 
+        previous = 0
+        for i in range(len(nums)):
+            previous = previous + nums[i]
+            for j in range(i+1, len(nums)):
+                
+                next = next + nums[j]
+                if(i == 0 and next == 0):
+                    return i
+                if(j == len(nums) - 1 and previous == 0):
+                    return j
+                
+            if(previous == next):
+                return i
+            next = 0
+            
+        return -1
+a = Solution()
+print(a.pivotIndex([1,7,3,6,5,6]))
