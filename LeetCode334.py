985614127926e4f1b684ebd14c825edc1d23b84c
Amir <amirhosseinpapaki@gmail.com>
2024-11-11 22:34:10 +0330

    Add two solution

--- LeetCode334.py
+++ LeetCode334.py
@@ -1,22 +1,9 @@
 class Solution:
     def increasingTriplet(self, nums: list[int]) -> bool:
-        counter =  0 
-        index = 1
-        compare = nums[0]
-        for i in range(1, len(nums) - 1):
-            if(nums[i] > compare):
-                compare = nums[i]
-                counter += 1
-                index += 1
-            while True:
-
-                if(index < i):
-                     compare = nums[i]
-                index += 1     
-            if(counter == 2):
-                return True
-            
-        return False
+        first =  float('inf')
+        secode = float('inf')
+        for i in range(len(nums)) :
+            if(i )
     
 a = Solution()
 print(a.increasingTriplet([1,2,1,3]))
--- LeetCode443.py
+++ LeetCode443.py
@@ -1,27 +1,22 @@
 class Solution:
-    def compress(self, chars: list[str]) -> int:
-        finalLen = 0
-        dynamicLen = 0
-        dynamicChar = chars[0]
-        if(chars[1] not in  dynamicChar):
-            finalLen = finalLen + 1
-        if(len(chars) == 1):
-            return 1
-        for j in range(1,len(chars)):
-            if(chars[j] in dynamicChar):
-                dynamicChar += chars[j]
-                if(len(dynamicChar) == 1):
-                    finalLen = finalLen + 1
-            else:  
-                if(finalLen > )
-                dynamicLen = 1 +len(str(len(dynamicChar)))
-                finalLen = dynamicLen + finalLen
-                dynamicLen = 0
-                dynamicChar = chars[j]
-            if(j == len(chars) -1):
-                dynamicLen = 1 +len(str(len(dynamicChar)))
-                finalLen = dynamicLen + finalLen
-        return finalLen
+  def compress(self, chars: list[str]) -> int:
+    ans = 0
+    i = 0
+
+    while i < len(chars):
+      letter = chars[i]
+      count = 0
+      while i < len(chars) and chars[i] == letter:
+        count += 1
+        i += 1
+      chars[ans] = letter
+      ans += 1
+      if count > 1:
+        for c in str(count):
+          chars[ans] = c
+          ans += 1
+
+    return ans
 
 a = Solution()
 print(a.compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]))
